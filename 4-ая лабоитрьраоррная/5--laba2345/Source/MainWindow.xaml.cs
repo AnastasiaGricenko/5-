3aaf41fc97af901d425e6f5a56b8b6b366204408
AnastasiaGricenko <115503674+AnastasiaGricenko@users.noreply.github.com>
2026-09-11 13:28:07 +0300

    4 лаболаторная
    Файлы только по четвёртой лабе

---
+++ 4-ая лабоитрьраоррная/5--laba2345/Source/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Windows;
+
+namespace DndAssistant
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new GMViewModel();
+        }
+    }
+}
